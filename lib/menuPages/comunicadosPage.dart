0c14ff1abccf898b66c14eac2be8848fdd4e1ccb
Vinícius Schneider <mrschneider.v@gmail.com>
2023-02-22 23:12:04 -0300

    ComunidadosPage

--- lib/menuPages/comunicadosPage.dart
+++ lib/menuPages/comunicadosPage.dart
@@ -1,6 +1,7 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:mermas_digitais_app/models/postModel.dart';
 
 class ComunicadosPage extends StatefulWidget {
   const ComunicadosPage({super.key});
@@ -13,45 +14,83 @@ class _ComunicadosPageState extends State<ComunicadosPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 3,
-          toolbarHeight: 70,
-          title: Text(
-            'Comunicados',
-            style: TextStyle(
-                color: Color.fromARGB(255, 221, 199, 248),
-                fontFamily: 'PaytoneOne',
-                //fontWeight: FontWeight.bold,
-                fontSize: 28),
-          ),
-          backgroundColor: const Color.fromARGB(255, 51, 0, 67),
+      appBar: AppBar(
+        elevation: 3,
+        toolbarHeight: 70,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(15),
+                bottomRight: Radius.circular(15))),
+        title: const Text(
+          'Comunicados',
+          style: TextStyle(
+              color: Color.fromARGB(255, 221, 199, 248),
+              fontFamily: 'PaytoneOne',
+              //fontWeight: FontWeight.bold,
+              fontSize: 28),
         ),
-        body: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                margin:
-                    EdgeInsets.only(left: 16, right: 16, bottom: 10, top: 20),
-                padding: EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                    color: Colors.deepPurple,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
+        backgroundColor: const Color.fromARGB(255, 51, 0, 67),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 20),
+          child: ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      //height: 130,
+                      child: Card(
+                        color: const Color.fromARGB(255, 221, 199, 248),
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    posts[index].PostTitle,
+                                    style: const TextStyle(
+                                        color: Color.fromARGB(255, 51, 0, 67),
+                                        fontFamily: "PaytoneOne",
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 10),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    posts[index].PostConteudo,
+                                    style: const TextStyle(
+                                      color: Color.fromARGB(255, 51, 0, 67),
+                                      fontFamily: "Poppins",
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 10,
-                        spreadRadius: 1,
-                      )
-                    ]),
-                child: Text("Card 1"),
-              ),
-            ],
-          ),
-        ));
+                  ],
+                );
+              }),
+        ),
+      ),
+    );
   }
 }
