da427c16b10ea3ad1eec26fb13ac334833f1df61
Vinícius Schneider <mrschneider.v@gmail.com>
2023-02-23 12:04:35 -0300

    CheckCurrentUser

--- lib/menuPages/frequenciaPage.dart
+++ lib/menuPages/frequenciaPage.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:mermas_digitais_app/models/loadingWindow.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
 class FrequenciaPage extends StatefulWidget {
@@ -34,107 +35,114 @@ class _FrequenciaPageState extends State<FrequenciaPage> {
                 fontSize: 28),
           ),
           backgroundColor: const Color.fromARGB(255, 51, 0, 67)),
-      body: SafeArea(
-        child: Card(
-          margin: const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
-          color: const Color.fromARGB(255, 221, 199, 248),
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  // ignore: prefer_const_literals_to_create_immutables
-                  children: [
-                    const Text(
-                      'Situação',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 51, 0, 67),
-                        fontFamily: "PaytoneOne",
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
+      body: FirebaseAuth.instance.currentUser != null
+          ? SafeArea(
+              child: Center(
+                child: Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  color: const Color.fromARGB(255, 221, 199, 248),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            const Text(
+                              'Situação',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 51, 0, 67),
+                                fontFamily: "PaytoneOne",
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 60),
+                        const Icon(
+                          Iconsax.personalcard,
+                          size: 120,
+                          color: Color.fromARGB(255, 51, 0, 67),
+                        ),
+                        Text(
+                          user.displayName != null
+                              ? user.displayName!
+                              : 'Lorem Ipsum da Silva',
+                          style: const TextStyle(
+                            color: Color.fromARGB(255, 51, 0, 67),
+                            fontFamily: "Poppins",
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 50),
+                        LinearPercentIndicator(
+                          barRadius: const Radius.circular(8),
+                          animation: true,
+                          animateFromLastPercent: true,
+                          animationDuration: 1000,
+                          lineHeight: 30,
+                          percent: 0.8,
+                          progressColor: const Color.fromARGB(255, 51, 0, 67),
+                          backgroundColor:
+                              const Color.fromARGB(255, 221, 199, 248),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: const [
+                            Text(
+                              'Você Possui 80% de presença nas aulas.',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 51, 0, 67),
+                                fontFamily: "Poppins",
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: const [
+                            Text(
+                              'Parabéns pelo seu empenho!',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 51, 0, 67),
+                                fontFamily: "Poppins",
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 100),
+                        TextButton(
+                          style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(5))),
+                              backgroundColor: MaterialStateProperty.all(
+                                  const Color.fromARGB(255, 51, 0, 67))),
+                          onPressed: () {},
+                          child: const Text(
+                            '   Detalhes   ',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 221, 199, 248),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                const SizedBox(height: 60),
-                const Icon(
-                  Iconsax.personalcard,
-                  size: 120,
-                  color: Color.fromARGB(255, 51, 0, 67),
-                ),
-                Text(
-                  user.displayName != null
-                      ? user.displayName!
-                      : 'Lorem Ipsum da Silva',
-                  style: const TextStyle(
-                    color: Color.fromARGB(255, 51, 0, 67),
-                    fontFamily: "Poppins",
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                const SizedBox(height: 50),
-                LinearPercentIndicator(
-                  barRadius: const Radius.circular(8),
-                  animation: true,
-                  animateFromLastPercent: true,
-                  animationDuration: 1000,
-                  lineHeight: 30,
-                  percent: 0.8,
-                  progressColor: const Color.fromARGB(255, 51, 0, 67),
-                  backgroundColor: const Color.fromARGB(255, 221, 199, 248),
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    Text(
-                      'Você Possui 80% de presença nas aulas.',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 51, 0, 67),
-                        fontFamily: "Poppins",
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    Text(
-                      'Parabéns pelo seu empenho!',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 51, 0, 67),
-                        fontFamily: "Poppins",
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-                const Expanded(child: Text('')),
-                TextButton(
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5))),
-                      backgroundColor: MaterialStateProperty.all(
-                          const Color.fromARGB(255, 51, 0, 67))),
-                  onPressed: () {},
-                  child: const Text(
-                    '   Detalhes   ',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 221, 199, 248),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+              ),
+            )
+          : const LoadingWindow(),
     );
   }
 }
