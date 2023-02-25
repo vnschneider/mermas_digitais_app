5ff7656d7cf71274cb64dfef37748a07e687ccd9
IsraelJorge <israeljorge2000@gmail.com>
2023-02-25 20:11:36 -0300

    add username on screens

--- lib/menuPages/perfilPage.dart
+++ lib/menuPages/perfilPage.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: file_names
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
@@ -17,150 +18,174 @@ class PerfilPage extends StatefulWidget {
 class _PerfilPageState extends State<PerfilPage> {
   final user = FirebaseAuth.instance.currentUser!;
 
+  final CollectionReference _users =
+      FirebaseFirestore.instance.collection('users');
+
+  Stream collectionStream =
+      FirebaseFirestore.instance.collection('users').snapshots();
+  Stream documentStream = FirebaseFirestore.instance
+      .collection('users')
+      .doc(FirebaseAuth.instance.currentUser!.uid)
+      .snapshots();
+
+  var value = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            elevation: 3,
-            toolbarHeight: 70,
-            shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(15),
-                    bottomRight: Radius.circular(15))),
-            title: const Text(
-              'Perfil',
-              style: TextStyle(
-                  color: Color.fromARGB(255, 221, 199, 248),
-                  fontFamily: 'PaytoneOne',
-                  //fontWeight: FontWeight.bold,
-                  fontSize: 28),
-            ),
-            backgroundColor: const Color.fromARGB(255, 51, 0, 67)),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
-            child: Card(
-              //margin: const EdgeInsets.only(bottom: 520),
-              color: const Color.fromARGB(255, 221, 199, 248),
+      appBar: AppBar(
+          elevation: 3,
+          toolbarHeight: 70,
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15))),
+          title: const Text(
+            'Perfil',
+            style: TextStyle(
+                color: Color.fromARGB(255, 221, 199, 248),
+                fontFamily: 'PaytoneOne',
+                //fontWeight: FontWeight.bold,
+                fontSize: 28),
+          ),
+          backgroundColor: const Color.fromARGB(255, 51, 0, 67)),
+      body: StreamBuilder(
+        stream: _users.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.hasData) {
+            final DocumentSnapshot documentSnapshot =
+                streamSnapshot.data!.docs[value];
+
+            return SafeArea(
               child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      Iconsax.personalcard,
-                      size: 90,
-                      color: Color.fromARGB(255, 51, 0, 67),
-                    ),
-                    const SizedBox(width: 10),
-                    Column(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+                child: Card(
+                  //margin: const EdgeInsets.only(bottom: 520),
+                  color: const Color.fromARGB(255, 221, 199, 248),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
                       mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text(
-                          FirebaseAuth.instance.currentUser != null
-                              ? 'Lorem Ipsum da Silva'
-                              : 'Lorem Ipsum da Silva',
-                          style: const TextStyle(
-                            color: Color.fromARGB(255, 51, 0, 67),
-                            fontFamily: "PaytoneOne",
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                          ),
+                        const Icon(
+                          Iconsax.personalcard,
+                          size: 90,
+                          color: Color.fromARGB(255, 51, 0, 67),
                         ),
-                        Text(
-                          user.email!,
-                          style: const TextStyle(
-                            color: Color.fromARGB(255, 51, 0, 67),
-                            fontFamily: "Poppins",
-                            fontSize: 16,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        GestureDetector(
-                          onTap: () {
-                            showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return const LoadingWindow();
-                                });
-                            Future.delayed(const Duration(milliseconds: 1000),
-                                () {
-                              Navigator.of(context).pop();
-                              FirebaseAuth.instance.signOut();
-                            });
-
-                            // ignore: use_build_context_synchronously
-                          },
-                          child: Row(
-                            // ignore: prefer_const_literals_to_create_immutables
-                            children: [
-                              const Icon(
-                                Iconsax.logout,
-                                size: 25,
+                        const SizedBox(width: 10),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              documentSnapshot['name'],
+                              style: const TextStyle(
                                 color: Color.fromARGB(255, 51, 0, 67),
-                                fill: 1,
+                                fontFamily: "PaytoneOne",
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
                               ),
-                              const SizedBox(width: 5),
-                              const Text(
-                                'Sair',
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 51, 0, 67),
-                                    fontFamily: "Poppins",
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        GestureDetector(
-                          onTap: () {
-                            showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return const LoadingWindow();
-                                });
-                            Future.delayed(const Duration(milliseconds: 1000),
-                                () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => UserInformation(),
-                              ));
-                            });
-
-                            // ignore: use_build_context_synchronously
-                          },
-                          child: Row(
-                            // ignore: prefer_const_literals_to_create_immutables
-                            children: [
-                              const Icon(
-                                Iconsax.document_upload,
-                                size: 25,
+                            ),
+                            Text(
+                              user.email!,
+                              style: const TextStyle(
                                 color: Color.fromARGB(255, 51, 0, 67),
-                                fill: 1,
+                                fontFamily: "Poppins",
+                                fontSize: 16,
                               ),
-                              const SizedBox(width: 5),
-                              const Text(
-                                'UpdateUser',
-                                style: TextStyle(
+                            ),
+                            const SizedBox(height: 10),
+                            GestureDetector(
+                              onTap: () {
+                                showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return const LoadingWindow();
+                                    });
+                                Future.delayed(
+                                    const Duration(milliseconds: 1000), () {
+                                  Navigator.of(context).pop();
+                                  FirebaseAuth.instance.signOut();
+                                });
+
+                                // ignore: use_build_context_synchronously
+                              },
+                              child: Row(
+                                // ignore: prefer_const_literals_to_create_immutables
+                                children: [
+                                  const Icon(
+                                    Iconsax.logout,
+                                    size: 25,
                                     color: Color.fromARGB(255, 51, 0, 67),
-                                    fontFamily: "Poppins",
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold),
+                                    fill: 1,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  const Text(
+                                    'Sair',
+                                    style: TextStyle(
+                                        color: Color.fromARGB(255, 51, 0, 67),
+                                        fontFamily: "Poppins",
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                        ),
+                            ),
+                            const SizedBox(height: 10),
+                            GestureDetector(
+                              onTap: () {
+                                showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return const LoadingWindow();
+                                    });
+                                Future.delayed(
+                                    const Duration(milliseconds: 1000), () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => UserInformation(),
+                                  ));
+                                });
+
+                                // ignore: use_build_context_synchronously
+                              },
+                              child: Row(
+                                // ignore: prefer_const_literals_to_create_immutables
+                                children: [
+                                  const Icon(
+                                    Iconsax.document_upload,
+                                    size: 25,
+                                    color: Color.fromARGB(255, 51, 0, 67),
+                                    fill: 1,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  const Text(
+                                    'UpdateUser',
+                                    style: TextStyle(
+                                        color: Color.fromARGB(255, 51, 0, 67),
+                                        fontFamily: "Poppins",
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )
                       ],
-                    )
-                  ],
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-        ));
+            );
+          }
+
+          return const LoadingWindow();
+        },
+      ),
+    );
   }
 }
