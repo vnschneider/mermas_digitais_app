bf717981ea311894b50a05c0bbbfa3eb44ea1766
Vinícius Schneider <mrschneider.v@gmail.com>
2023-02-26 02:25:46 -0300

    Update checkLogin.dart

--- lib/services/checkLogin.dart
+++ lib/services/checkLogin.dart
@@ -9,17 +9,19 @@ class CheckLogin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return const HomePage();
-          } else {
-            return const LoginPage();
-          }
-        },
-      ),
-    );
+    return FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) => Scaffold(
+              body: StreamBuilder<User?>(
+                stream: FirebaseAuth.instance.authStateChanges(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return const HomePage();
+                  } else {
+                    return const LoginPage();
+                  }
+                },
+              ),
+            ));
   }
 }
