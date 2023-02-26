7d4eab583e5c05da5fe7027a3ffca5641830eacf
Vinícius Schneider <mrschneider.v@gmail.com>
2023-02-25 21:59:10 -0300

    move pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'loginPages/checkLogin.dart';
+import 'services/firebase_options.dart';
+import 'services/checkLogin.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
---
+++ lib/services/checkLogin.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: file_names
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import '../menuPages/homePage.dart';
+import '../loginPages/loginPage.dart';
+
+class CheckLogin extends StatelessWidget {
+  const CheckLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const HomePage();
+          } else {
+            return const LoginPage();
+          }
+        },
+      ),
+    );
+  }
+}
