4668085f00c74502b2a851a74166008877c1d543
Vinícius Schneider <mrschneider.v@gmail.com>
2023-02-26 02:17:11 -0300

    Update checkLogin.dart

--- lib/services/checkLogin.dart
+++ lib/services/checkLogin.dart
@@ -1,13 +1,16 @@
-// ignore_for_file: file_names
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../menuPages/homePage.dart';
 import '../loginPages/loginPage.dart';
 
-class CheckLogin extends StatelessWidget {
+class CheckLogin extends StatefulWidget {
   const CheckLogin({super.key});
 
+  @override
+  State<CheckLogin> createState() => _CheckLoginState();
+}
+
+class _CheckLoginState extends State<CheckLogin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
