98b7f300cef62eaafa9482b13f654acbcf94e972
Vinícius Schneider <mrschneider.v@gmail.com>
2023-03-16 23:41:19 -0300

    Recuperando função uploadImage
    Retomei o uso da função "uploadImage" para adicionar foto de perfil ao usuário. Também adicionei um FutureBuilder para exibir os dados do usuário nessa tela, alguns deles não serão editáveis pelo usuário. As demais alterações no códigos são apenas estéticas!

--- lib/src/screens/loginPages/new_user_page.dart
+++ lib/src/screens/loginPages/new_user_page.dart
@@ -1,6 +1,8 @@
 import 'package:mermas_digitais_app/core/exports/login_page_exports.dart';
 import 'package:mermas_digitais_app/core/exports/new_user_exports.dart';
 
+import '../../functions/get_user_info.dart';
+
 class NewUserPage extends StatefulWidget {
   const NewUserPage({super.key});
 
@@ -14,6 +16,7 @@ class _NewUserPageState extends State<NewUserPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
+  GetUserInfo userInfo = GetUserInfo();
   var userProfilePhoto = '';
   String userUID = '';
   String userEmail = '';
@@ -41,6 +44,27 @@ class _NewUserPageState extends State<NewUserPage> {
     print(user.currentUser!.uid);
   }
 
+  void uploadImage() async {
+    try {
+      final profilePhoto = await ImagePicker()
+          .pickImage(source: ImageSource.gallery, imageQuality: 50);
+
+      final profilephotoRef = FirebaseStorage.instance
+          .ref()
+          .child('users/${userInfo.user.uid}/profilephoto.jpg');
+
+      await profilephotoRef.putFile(File(profilePhoto!.path));
+      profilephotoRef.getDownloadURL().then((value) {
+        setState(() {
+          userProfilePhoto = value;
+        });
+        print(value);
+      });
+    } catch (e) {
+      print("O processo falhou: $e");
+    }
+  }
+
   void showPassword() {
     setState(() {
       _showPassword == false ? _showPassword = true : _showPassword = false;
@@ -58,193 +82,214 @@ class _NewUserPageState extends State<NewUserPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 51, 0, 67),
-      body: ListView(
-        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-        shrinkWrap: true,
-        reverse: true,
-        padding:
-            const EdgeInsets.only(top: 60, right: 20, left: 20, bottom: 20),
-        children: [
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const OlaMerma(
-                  title: 'Conclua seu cadastro',
-                  usetext: false,
-                  text: '',
-                ),
-
-                //ProfilePhoto Container
-
-                GestureDetector(
-                  onTap: () {
-                    // uploadImage();
-                  },
-                  child: userProfilePhoto != ''
-                      ? CircleAvatar(
-                          radius: 60,
-                          backgroundImage: NetworkImage(userProfilePhoto))
-                      : const CircleAvatar(
-                          radius: 60,
-                          child: Icon(
-                            Iconsax.personalcard,
-                            size: 120,
-                            color: Color.fromARGB(255, 221, 199, 248),
-                          ),
-                        ),
-                ),
-                const SizedBox(height: 30),
-                //UID TextField
-                CustomTextField(
-                  controller: _nameController,
-                  hintText: "Nome",
-                ),
-                CustomTextField(
-                  controller: _emailController,
-                  hintText: "Email",
-                ),
-                //Password TextField
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 51, 0, 67),
-                      border: Border.all(
-                          color: const Color.fromARGB(255, 221, 199, 248)),
-                      borderRadius: BorderRadius.circular(12),
+    return FutureBuilder(
+      future: userInfo.getUserInfo(),
+      builder: (context, snapshot) => Scaffold(
+        backgroundColor: const Color.fromARGB(255, 51, 0, 67),
+        body: ListView(
+          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+          shrinkWrap: true,
+          reverse: true,
+          padding:
+              const EdgeInsets.only(top: 60, right: 20, left: 20, bottom: 20),
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20),
+                  const FittedBox(
+                    fit: BoxFit.contain,
+                    child: Text(
+                      'Conclua seu cadastro',
+                      textAlign: TextAlign.center,
+                      maxLines: 1,
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 221, 199, 248),
+                          fontFamily: 'PaytoneOne',
+                          //fontWeight: FontWeight.bold,
+                          fontSize: 30),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: TextField(
-                        controller: _passwordController,
-                        obscureText: _showPassword,
-                        decoration: InputDecoration(
-                          suffixIcon: GestureDetector(
-                            onTap: () {
-                              showPassword();
-                            },
+                  ),
+                  const SizedBox(height: 60),
+                  //ProfilePhoto Container
+
+                  GestureDetector(
+                    onTap: () {
+                      uploadImage();
+                    },
+                    child: userInfo.userProfilePhoto != ''
+                        ? CircleAvatar(
+                            radius: 80,
+                            backgroundImage:
+                                NetworkImage(userInfo.userProfilePhoto))
+                        : const CircleAvatar(
+                            radius: 60,
                             child: Icon(
-                              _showPassword == true
-                                  ? Iconsax.eye_slash
-                                  : Iconsax.eye,
-                              color: const Color.fromARGB(200, 221, 199, 248),
+                              Iconsax.personalcard,
+                              size: 120,
+                              color: Color.fromARGB(255, 221, 199, 248),
                             ),
                           ),
-                          border: InputBorder.none,
-                          hintText: 'Senha',
-                          hintStyle: const TextStyle(
-                              fontFamily: 'Poppins',
-                              color: Color.fromARGB(255, 221, 199, 248)),
-                        ),
-                      ),
-                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
+                  const SizedBox(height: 30),
+                  //UID TextField
 
-                //Confirm Password TextField
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 51, 0, 67),
-                      border: Border.all(
-                          color: const Color.fromARGB(255, 221, 199, 248)),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: TextField(
-                        controller: _confirmPasswordController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Confirmar senha',
-                          hintStyle: TextStyle(
-                              fontFamily: 'Poppins',
-                              color: Color.fromARGB(255, 221, 199, 248)),
-                        ),
-                      ),
-                    ),
+                  CustomTextField(
+                    useController: true,
+                    enabled: true,
+                    controller: _nameController,
+                    hintText: 'Nome',
                   ),
-                ),
-                const SizedBox(height: 40),
-
-                //RegisterButton
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 100),
-                  child: GestureDetector(
-                    onTap: () {
-                      if (_emailController.text.isNotEmpty &&
-                          _passwordController.text.isNotEmpty &&
-                          _confirmPasswordController.text.isNotEmpty) {
-                        showDialog(
-                            context: context,
-                            builder: (context) {
-                              return const LoadingWindow();
-                            });
-                        newUser().whenComplete(() {
-                          createUserDB(_nameController.text.trim());
-                          Navigator.pushNamed(context, "login");
-                        });
-                      } else {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return const AlertDialog(
-                              backgroundColor:
-                                  Color.fromARGB(255, 221, 199, 248),
-                              title: Text(
-                                "Algo deu errado!",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w500,
-                                  color: Color.fromARGB(255, 51, 0, 67),
-                                ),
-                              ),
-                              content: Text(
-                                "Tenha certeza de que preencheu os campos corretamente!",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                  color: Color.fromARGB(255, 51, 0, 67),
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                      }
-                    },
+                  CustomTextField(
+                    useController: false,
+                    enabled: false,
+                    controller: _emailController,
+                    hintText: userInfo.userEmail,
+                  ),
+                  //Password TextField
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
                     child: Container(
-                      padding: const EdgeInsets.all(10),
                       decoration: BoxDecoration(
                         color: const Color.fromARGB(255, 51, 0, 67),
                         border: Border.all(
                             color: const Color.fromARGB(255, 221, 199, 248)),
                         borderRadius: BorderRadius.circular(12),
                       ),
-                      child: const Center(
-                        child: Text(
-                          'Cadastrar',
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 221, 199, 248),
-                              fontFamily: 'Poppins',
-                              //fontWeight: FontWeight.bold,
-                              fontSize: 18),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: TextField(
+                          controller: _passwordController,
+                          obscureText: _showPassword,
+                          decoration: InputDecoration(
+                            suffixIcon: GestureDetector(
+                              onTap: () {
+                                showPassword();
+                              },
+                              child: Icon(
+                                _showPassword == true
+                                    ? Iconsax.eye_slash
+                                    : Iconsax.eye,
+                                color: const Color.fromARGB(200, 221, 199, 248),
+                              ),
+                            ),
+                            border: InputBorder.none,
+                            hintText: 'Senha',
+                            hintStyle: const TextStyle(
+                                fontFamily: 'Poppins',
+                                color: Color.fromARGB(255, 221, 199, 248)),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 20),
+
+                  //Confirm Password TextField
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 51, 0, 67),
+                        border: Border.all(
+                            color: const Color.fromARGB(255, 221, 199, 248)),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: TextField(
+                          controller: _confirmPasswordController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Confirmar senha',
+                            hintStyle: TextStyle(
+                                fontFamily: 'Poppins',
+                                color: Color.fromARGB(255, 221, 199, 248)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+
+                  //RegisterButton
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 100),
+                    child: GestureDetector(
+                      onTap: () {
+                        if (_nameController.text.isNotEmpty &&
+                            _passwordController.text.isNotEmpty &&
+                            _confirmPasswordController.text.isNotEmpty &&
+                            userProfilePhoto.isNotEmpty) {
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return const LoadingWindow();
+                              });
+                          newUser().whenComplete(() {
+                            createUserDB(_nameController.text.trim());
+                            Navigator.pushNamed(context, "login");
+                          });
+                        } else {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return const AlertDialog(
+                                backgroundColor:
+                                    Color.fromARGB(255, 221, 199, 248),
+                                title: Text(
+                                  "Algo deu errado!",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontFamily: "Poppins",
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromARGB(255, 51, 0, 67),
+                                  ),
+                                ),
+                                content: Text(
+                                  "Tenha certeza de que preencheu os campos corretamente e adicionou uma foto de perfil.",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontFamily: "Poppins",
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color.fromARGB(255, 51, 0, 67),
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        }
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 51, 0, 67),
+                          border: Border.all(
+                              color: const Color.fromARGB(255, 221, 199, 248)),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Cadastrar',
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 221, 199, 248),
+                                fontFamily: 'Poppins',
+                                //fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
