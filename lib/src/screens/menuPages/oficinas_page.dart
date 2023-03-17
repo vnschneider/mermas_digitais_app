f0eafe65196938c5b3a704442fced0baf51895b3
Vinícius Schneider <mrschneider.v@gmail.com>
2023-03-17 20:48:50 -0300

    LaunchURL & collection db class & adminMenu

--- lib/src/screens/menuPages/oficinas_page.dart
+++ lib/src/screens/menuPages/oficinas_page.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:mermas_digitais_app/src/models/oficina_model.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../../core/exports/frequencia_exports.dart';
+import '../../functions/get_user_info.dart';
+import '../../models/newClass_window/newClass_window.dart';
 
 class OficinasPage extends StatefulWidget {
   const OficinasPage({super.key});
@@ -9,136 +12,192 @@ class OficinasPage extends StatefulWidget {
 }
 
 class _OficinasPageState extends State<OficinasPage> {
+  GetUserInfo userInfo = GetUserInfo();
+
+  _launchUrl(Uri url) async {
+    if (await launchUrl(url, mode: LaunchMode.externalApplication)) {
+      await canLaunchUrl(url);
+    } else {
+      print("Não foi possível abrir o link");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          elevation: 3,
-          toolbarHeight: 70,
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(15),
-                  bottomRight: Radius.circular(15))),
-          title: const Text(
-            'Oficinas',
-            style: TextStyle(
-                color: Color.fromARGB(255, 221, 199, 248),
-                fontFamily: 'PaytoneOne',
-                //fontWeight: FontWeight.bold,
-                fontSize: 28),
-          ),
-          backgroundColor: const Color.fromARGB(255, 51, 0, 67)),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 20),
-          child: ListView.builder(
-              itemCount: oficinas.length,
-              itemBuilder: (context, index) {
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      width: double.infinity,
-                      //height: 130,
-                      child: Card(
-                        color: const Color.fromARGB(255, 221, 199, 248),
-                        child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    oficinas[index].oficinaTitle,
-                                    style: const TextStyle(
-                                        color: Color.fromARGB(255, 51, 0, 67),
-                                        fontFamily: "PaytoneOne",
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(height: 10),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Expanded(
-                                    child: Text(
-                                      maxLines: 3,
-                                      textAlign: TextAlign.start,
-                                      overflow: TextOverflow.ellipsis,
-                                      oficinas[index].oficinaDescri,
-                                      style: const TextStyle(
-                                        color: Color.fromARGB(255, 51, 0, 67),
-                                        fontFamily: "Poppins",
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(height: 20),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  TextButton(
-                                    style: ButtonStyle(
-                                        shape: MaterialStateProperty.all<
-                                                RoundedRectangleBorder>(
-                                            RoundedRectangleBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(5))),
-                                        backgroundColor:
-                                            MaterialStateProperty.all(
-                                                const Color.fromARGB(
-                                                    255, 51, 0, 67))),
-                                    onPressed: () {},
-                                    child: const Text(
-                                      'Material de apoio',
-                                      style: TextStyle(
-                                        color:
-                                            Color.fromARGB(255, 221, 199, 248),
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(width: 10),
-                                  TextButton(
-                                    style: ButtonStyle(
-                                        shape: MaterialStateProperty.all<
-                                                RoundedRectangleBorder>(
-                                            RoundedRectangleBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(5))),
-                                        backgroundColor:
-                                            MaterialStateProperty.all(
-                                                const Color.fromARGB(
-                                                    255, 51, 0, 67))),
-                                    onPressed: () {},
-                                    child: const Text(
-                                      'Monitores',
-                                      style: TextStyle(
-                                        color:
-                                            Color.fromARGB(255, 221, 199, 248),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
+    return FutureBuilder(
+      future: userInfo.getUserInfo(),
+      builder: (context, snapshot) => StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('class').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) => !snapshot
+                .hasData
+            ? const LoadingWindow()
+            : Scaffold(
+                appBar: AppBar(
+                    elevation: 3,
+                    toolbarHeight: 70,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(15),
+                            bottomRight: Radius.circular(15))),
+                    title: const Text(
+                      'Oficinas',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 221, 199, 248),
+                          fontFamily: 'PaytoneOne',
+                          //fontWeight: FontWeight.bold,
+                          fontSize: 28),
                     ),
-                  ],
-                );
-              }),
-        ),
+                    backgroundColor: const Color.fromARGB(255, 51, 0, 67)),
+                body: SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 12, vertical: 20),
+                    child: ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          final DocumentSnapshot doc =
+                              snapshot.data!.docs[index];
+                          return Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              const SizedBox(height: 20),
+                              SizedBox(
+                                width: double.infinity,
+                                //height: 130,
+                                child: Card(
+                                  color:
+                                      const Color.fromARGB(255, 221, 199, 248),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(20.0),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              doc['classTitle'].toString(),
+                                              style: const TextStyle(
+                                                  color: Color.fromARGB(
+                                                      255, 51, 0, 67),
+                                                  fontFamily: "PaytoneOne",
+                                                  fontSize: 20,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ],
+                                        ),
+                                        const SizedBox(height: 10),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Expanded(
+                                              child: Text(
+                                                maxLines: 3,
+                                                textAlign: TextAlign.start,
+                                                overflow: TextOverflow.ellipsis,
+                                                doc['classContent'].toString(),
+                                                style: const TextStyle(
+                                                  color: Color.fromARGB(
+                                                      255, 51, 0, 67),
+                                                  fontFamily: "Poppins",
+                                                  fontSize: 14,
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        const SizedBox(height: 20),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          children: [
+                                            TextButton(
+                                              style: ButtonStyle(
+                                                  shape: MaterialStateProperty
+                                                      .all<RoundedRectangleBorder>(
+                                                          RoundedRectangleBorder(
+                                                              borderRadius:
+                                                                  BorderRadius
+                                                                      .circular(
+                                                                          5))),
+                                                  backgroundColor:
+                                                      MaterialStateProperty.all(
+                                                          const Color.fromARGB(
+                                                              255, 51, 0, 67))),
+                                              onPressed: () {
+                                                _launchUrl(Uri.parse(
+                                                    doc['classLink']
+                                                        .toString()));
+                                              },
+                                              child: const Text(
+                                                'Material de apoio',
+                                                style: TextStyle(
+                                                  color: Color.fromARGB(
+                                                      255, 221, 199, 248),
+                                                ),
+                                              ),
+                                            ),
+                                            const SizedBox(width: 10),
+                                            TextButton(
+                                              style: ButtonStyle(
+                                                  shape: MaterialStateProperty
+                                                      .all<RoundedRectangleBorder>(
+                                                          RoundedRectangleBorder(
+                                                              borderRadius:
+                                                                  BorderRadius
+                                                                      .circular(
+                                                                          5))),
+                                                  backgroundColor:
+                                                      MaterialStateProperty.all(
+                                                          const Color.fromARGB(
+                                                              255, 51, 0, 67))),
+                                              onPressed: () {},
+                                              child: const Text(
+                                                'Monitores',
+                                                style: TextStyle(
+                                                  color: Color.fromARGB(
+                                                      255, 221, 199, 248),
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        }),
+                  ),
+                ),
+                floatingActionButton: userInfo.userStatus == 'Admin'
+                    ? FloatingActionButton(
+                        elevation: 2,
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return const NewOficinaWindow();
+                            },
+                          );
+                        },
+                        child: const Icon(
+                          Iconsax.additem,
+                          size: 40,
+                        ))
+                    : null,
+              ),
       ),
     );
   }
