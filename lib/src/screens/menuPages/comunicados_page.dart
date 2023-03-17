5ef1d3848f7873428961b18cfafadef9b1dfc2da
Vinícius Schneider <mrschneider.v@gmail.com>
2023-03-17 20:48:27 -0300

    LaunchURL & collection db posts & adminMenu

--- lib/src/screens/menuPages/comunicados_page.dart
+++ lib/src/screens/menuPages/comunicados_page.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:mermas_digitais_app/src/models/post_model.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../../core/exports/frequencia_exports.dart';
+import '../../functions/get_user_info.dart';
+import '../../models/newPost_window/newPost_window.dart';
 
 class ComunicadosPage extends StatefulWidget {
   const ComunicadosPage({super.key});
@@ -9,87 +12,170 @@ class ComunicadosPage extends StatefulWidget {
 }
 
 class _ComunicadosPageState extends State<ComunicadosPage> {
+  GetUserInfo userInfo = GetUserInfo();
+
+  _launchUrl(Uri url) async {
+    if (await launchUrl(url, mode: LaunchMode.externalApplication)) {
+      await canLaunchUrl(url);
+    } else {
+      print("Não foi possível abrir o link");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 3,
-        toolbarHeight: 70,
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(15),
-                bottomRight: Radius.circular(15))),
-        title: const Text(
-          'Comunicados',
-          style: TextStyle(
-              color: Color.fromARGB(255, 221, 199, 248),
-              fontFamily: 'PaytoneOne',
-              //fontWeight: FontWeight.bold,
-              fontSize: 28),
-        ),
-        backgroundColor: const Color.fromARGB(255, 51, 0, 67),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 40),
-          child: ListView.builder(
-              itemCount: posts.length,
-              itemBuilder: (context, index) {
-                return Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 20),
-                    Card(
-                      color: const Color.fromARGB(255, 221, 199, 248),
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  posts[index].postTitle,
-                                  style: const TextStyle(
-                                      color: Color.fromARGB(255, 51, 0, 67),
-                                      fontFamily: "PaytoneOne",
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 10),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                    maxLines: 3,
-                                    textAlign: TextAlign.start,
-                                    overflow: TextOverflow.ellipsis,
-                                    posts[index].postConteudo,
-                                    style: const TextStyle(
-                                      color: Color.fromARGB(255, 51, 0, 67),
-                                      fontFamily: "Poppins",
-                                      fontSize: 14,
-                                    ),
+    return FutureBuilder(
+      future: userInfo.getUserInfo(),
+      builder: (context, snapshot) => StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) => !snapshot
+                .hasData
+            ? const LoadingWindow()
+            : Scaffold(
+                appBar: AppBar(
+                  elevation: 3,
+                  toolbarHeight: 70,
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(15),
+                          bottomRight: Radius.circular(15))),
+                  title: const Text(
+                    'Comunicados',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 221, 199, 248),
+                        fontFamily: 'PaytoneOne',
+                        //fontWeight: FontWeight.bold,
+                        fontSize: 28),
+                  ),
+                  backgroundColor: const Color.fromARGB(255, 51, 0, 67),
+                ),
+                body: SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 12, vertical: 40),
+                    child: ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          final DocumentSnapshot doc =
+                              snapshot.data!.docs[index];
+                          return Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              const SizedBox(height: 20),
+                              Card(
+                                color: const Color.fromARGB(255, 221, 199, 248),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(20.0),
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            doc['postTitle'].toString(),
+                                            style: const TextStyle(
+                                                color: Color.fromARGB(
+                                                    255, 51, 0, 67),
+                                                fontFamily: "PaytoneOne",
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ],
+                                      ),
+                                      const SizedBox(height: 10),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Expanded(
+                                            child: Text(
+                                              doc['postContent'].toString(),
+                                              maxLines: 3,
+                                              textAlign: TextAlign.start,
+                                              overflow: TextOverflow.ellipsis,
+                                              style: const TextStyle(
+                                                color: Color.fromARGB(
+                                                    255, 51, 0, 67),
+                                                fontFamily: "Poppins",
+                                                fontSize: 14,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      const SizedBox(height: 10),
+                                      doc['postLink'].toString().isNotEmpty
+                                          ? Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.start,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                GestureDetector(
+                                                  onTap: () {
+                                                    if (doc['postLink']
+                                                        .toString()
+                                                        .isNotEmpty) {
+                                                      _launchUrl(Uri.parse(
+                                                          doc['postLink']
+                                                              .toString()));
+                                                    }
+                                                  },
+                                                  child: const Text(
+                                                    'Mais sobre',
+                                                    maxLines: 1,
+                                                    textAlign: TextAlign.start,
+                                                    overflow:
+                                                        TextOverflow.ellipsis,
+                                                    style: TextStyle(
+                                                      color: Color.fromARGB(
+                                                          255, 51, 0, 67),
+                                                      fontFamily: "Poppins",
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      fontSize: 12,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            )
+                                          : const SizedBox()
+                                    ],
                                   ),
                                 ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                );
-              }),
-        ),
+                              ),
+                            ],
+                          );
+                        }),
+                  ),
+                ),
+                floatingActionButton: userInfo.userStatus == 'Admin'
+                    ? FloatingActionButton(
+                        elevation: 2,
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return const NewPostWindow();
+                            },
+                          );
+                        },
+                        child: const Icon(
+                          Iconsax.additem,
+                          size: 40,
+                        ))
+                    : null,
+              ),
       ),
     );
   }
