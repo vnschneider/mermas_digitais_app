d0435bc87607673c45230498187415cfa023646f
Vinícius Schneider <mrschneider.v@gmail.com>
2023-03-16 23:33:34 -0300

    Alterando initialRoute para 'auth'

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: themeData,
       routes: routes,
-      initialRoute: 'newUser',
+      initialRoute: 'auth',
       debugShowCheckedModeBanner: false,
     );
   }
