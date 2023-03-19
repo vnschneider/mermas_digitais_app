e0fcf5abeb031d2cb9631afcb2c6d82b7eecb7c1
Vinícius Schneider <mrschneider.v@gmail.com>
2023-03-19 19:38:05 -0300

    push notifications att

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() async {
     options: DefaultFirebaseOptions.web,
   );
   final fcmToken = await FirebaseMessaging.instance.getToken();
-  print(fcmToken);
+  print('fcmToken: $fcmToken');
   runApp(const MyApp());
 }
 
