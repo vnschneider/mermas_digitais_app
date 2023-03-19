86e72815bb8bb21b83829b07d26f3366d606240d
Vinícius Schneider <mrschneider.v@gmail.com>
2023-03-18 21:48:26 -0300

    remove unnecessary Scaffold to errorAlertDialogWindow

--- lib/src/models/errorAlertDialog/errorAlertDialog.dart
+++ lib/src/models/errorAlertDialog/errorAlertDialog.dart
@@ -11,29 +11,27 @@ class ErrorAlertDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: AlertDialog(
-          backgroundColor: const Color.fromARGB(255, 221, 199, 248),
-          title: Text(
-            title,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              fontFamily: "Poppins",
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 51, 0, 67),
-            ),
+    return Center(
+      child: AlertDialog(
+        backgroundColor: const Color.fromARGB(255, 221, 199, 248),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontFamily: "Poppins",
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 51, 0, 67),
           ),
-          content: Text(
-            content,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              fontFamily: "Poppins",
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
-              color: Color.fromARGB(255, 51, 0, 67),
-            ),
+        ),
+        content: Text(
+          content,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontFamily: "Poppins",
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            color: Color.fromARGB(255, 51, 0, 67),
           ),
         ),
       ),
