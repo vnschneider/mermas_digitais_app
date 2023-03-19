618b37003d2f33a7375624dd0232f283fbd8062d
Vinícius Schneider <mrschneider.v@gmail.com>
2023-03-19 14:35:02 -0300

    Create showToastMessage

---
+++ lib/src/models/showToastMessage.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: file_names
+
+import 'dart:ui';
+import 'package:fluttertoast/fluttertoast.dart';
+
+Future showToastMessage({required String message}) async {
+  await Fluttertoast.cancel();
+
+  Fluttertoast.showToast(
+    toastLength: Toast.LENGTH_LONG,
+    gravity: ToastGravity.BOTTOM,
+    textColor: const Color.fromARGB(255, 51, 0, 67),
+    backgroundColor: const Color.fromARGB(255, 221, 199, 248),
+    msg: message,
+    fontSize: 18,
+  );
+}
