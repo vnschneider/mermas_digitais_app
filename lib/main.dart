2c3aa16489f9c738ae2e0bbd0f36f2214b1b4ad8
Vinícius Schneider <mrschneider.v@gmail.com>
2023-03-21 06:32:24 -0300

    get FCM Tocken removed
    Essa função requer um login recente, estáva atrapalhando o funcionamento do app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:mermas_digitais_app/core/firebase/firebase_options.dart';
@@ -10,8 +9,8 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.web,
   );
-  final fcmToken = await FirebaseMessaging.instance.getToken();
-  print('fcmToken: $fcmToken');
+  //final fcmToken = await FirebaseMessaging.instance.getToken();
+  //print('fcmToken: $fcmToken');
   runApp(const MyApp());
 }
 
