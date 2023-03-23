dc2be848ad0fcb3107290d3ede01d044e49160c3
Vinícius Schneider <mrschneider.v@gmail.com>
2023-03-23 15:09:18 -0300

    start create frequence list window

---
+++ lib/src/models/frequence_windows/frequence_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FrequenceList extends StatefulWidget {
+  const FrequenceList({super.key});
+
+  @override
+  State<FrequenceList> createState() => _FrequenceListState();
+}
+
+class _FrequenceListState extends State<FrequenceList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Olá mundo')),
+    );
+  }
+}
+
+
